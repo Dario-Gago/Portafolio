601c178a8b24b6c0e08048a2a3736340586beec3
Dario <gagodario1@gmail.com>
2025-08-30 22:33:45 -0400

    feat Vercel-2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <Router basename="/Portafolio">
+      <Router>
         <Header></Header>
         <Routes>
           <Route path="/" element={<Home />} />
